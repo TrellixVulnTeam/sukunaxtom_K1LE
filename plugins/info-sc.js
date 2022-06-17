93a22d8409baf0a2397db1fa36323a92f8e58c70
BernekelB <tommyheneldra@gmail.com>
2022-06-17 16:56:11 +0700

    sukunaxtom

---
+++ plugins/info-sc.js
@@ -0,0 +1,24 @@
+/*
+ * ngelarang lu biar g diubah kyknya emg susah ya anjg
+ * jadi kalau lu g bsa ngehargain orang
+ * setidaknya buat sc ori by gw, trs buat sc lu hasil copas tuh
+ * better bgtu dripada nma gw dihapus smua
+ * najis 👎🗿
+*/
+
+let handler = async (m, { conn }) => {
+let ye = `@${m.sender.split`@`[0]}`
+let esce = `
+Halo ${ye} Lagi Nyari Sc Kak? 
+
+Nih Link Sc Nya Dibawah :
+https://youtube.com/channel/UCZb42n4GYAqDRtGMew3rDfg
+
+_*Jangan Lupa Di Subscribe*_`
+conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
+}
+handler.help = ['sc', 'sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc|sourcecode)$/i
+
+module.exports = handler
